9d130ee355f43cff8271d928ac0347ba57f83c0a
Abhijeet Anand <abhijeetanand628@gmail.com>
2026-08-17 22:56:32 +0530

    Added AWS components and removed .env from tracking

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import {User} from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { uploadToS3 } from "../utils/s3.js";
+import { logToCloudWatch } from "../utils/cloudwatch.js";
 
 export const registerUser = async(req, res) => {
     try {
@@ -33,12 +35,13 @@ export const registerUser = async(req, res) => {
         // 4. Encrypt the password
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // 5. Upload profile image to Cloudinary (OPTIONAL)
+        // 5. Upload profile image to S3 (AWS Tier 1 Integration)
         const profileImagePath = req.files?.profileImage?.[0]?.path;
         let profileImageUrl = "";
 
         if(profileImagePath) {
-            const profileImage = await uploadOnCloudinary(profileImagePath);
+            // Replaced uploadOnCloudinary with AWS S3 integration
+            const profileImage = await uploadToS3(profileImagePath);
             if(profileImage) {
                 profileImageUrl = profileImage.url;
             }
@@ -52,6 +55,9 @@ export const registerUser = async(req, res) => {
             ...(profileImageUrl && { profileImage: profileImageUrl })
         })
 
+        // Send audit log to AWS CloudWatch
+        await logToCloudWatch("UserAuth", { event: "UserRegistered", email: user.email, userId: user._id });
+
         // 7. Send response
         return res
         .status(201)
@@ -62,6 +68,7 @@ export const registerUser = async(req, res) => {
 
     } catch (error) {
         console.log("Signup error : ", error);
+        await logToCloudWatch("UserAuth_Errors", { event: "RegistrationError", error: error.message });
         return res
         .status(500)
         .json({
@@ -117,6 +124,9 @@ export const loginUser = async(req, res) => {
             {expiresIn: process.env.JWT_EXPIRES_IN}
         );
 
+        // Send audit log to AWS CloudWatch
+        await logToCloudWatch("UserAuth", { event: "UserLoggedIn", email: user.email, userId: user._id });
+
         // 6. Send response
         return res
         .status(200)
@@ -132,6 +142,7 @@ export const loginUser = async(req, res) => {
 
     } catch (error) {
         console.log("Login error : ", error);
+        await logToCloudWatch("UserAuth_Errors", { event: "LoginError", error: error.message });
         res.status(500)
         .json({
             message: "Server error"
